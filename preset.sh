6175e903e85ba1bb2044802528d8731b1efcb51f
Philip <naekobest@users.noreply.github.com>
2025-11-24 19:51:39 +0100

    Update preset.sh

--- preset.sh
+++ preset.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Ordner erstellen
+# ZFS Pakete installieren (Debian/Ubuntu Beispiel)
+apt update
+apt install -y zfsutils-linux
+
+# Danach wie gehabt Ordner erstellen, fstab anpassen, mounten
 mkdir -p /mnt/UNAS-{Docker,Data,Media,Photos}
 
-# fstab-Einträge ergänzen
 for name in Docker Data Media Photos; do
   entry="10.10.1.3:/var/nfs/shared/$(echo $name | tr '[:upper:]' '[:lower:]') /mnt/UNAS-$name nfs defaults 0 0"
   if ! grep -q "$entry" /etc/fstab; then
@@ -11,7 +14,6 @@ for name in Docker Data Media Photos; do
   fi
 done
 
-# Mounten der NFS-Shares
 for name in Docker Data Media Photos; do
   mount /mnt/UNAS-$name
 done
