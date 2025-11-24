a9d12c897ed7d60eb5e8601765c7450d61bc774b
Philip <naekobest@users.noreply.github.com>
2025-11-24 19:53:34 +0100

    Update preset.sh

--- preset.sh
+++ preset.sh
@@ -5,7 +5,7 @@ apt update
 apt install -y zfsutils-linux
 
 # Danach wie gehabt Ordner erstellen, fstab anpassen, mounten
-mkdir -p /mnt/UNAS-{Docker,Data,Media,Photos}
+mkdir -p /mnt/pve/UNAS-{Docker,Data,Media,Photos}
 
 for name in Docker Data Media Photos; do
   entry="10.10.1.3:/var/nfs/shared/$(echo $name | tr '[:upper:]' '[:lower:]') /mnt/UNAS-$name nfs defaults 0 0"
